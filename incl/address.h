e3c07b66a218d5c373552a09ae2008d89914dad9
satyaprakash roy <satyaprakashroy28@gmail.com>
2024-01-22 22:52:26 +0530

    working on inter proces communication through file handeling

--- incl/address.h
+++ incl/address.h
@@ -1,7 +1,11 @@
 
+
 char *address( char * local_add ){
-	char r_val[add_size]=maindir;
+	char r_val[add_size] = maindir;
 	strcat(r_val,local_add);
 	char *rr = r_val;
 	return rr;
 }
+
+
+
--- incl/stream.c
+++ incl/stream.c
@@ -1,8 +1,19 @@
 #include "def.h"
 
-int main(){	
-	printf("%s",address("/this"));
+char * signal(char * addr);
+
+int main(){
+	char * me = address("/scipts/file\n");
+	printf("%s",me);
+	printf("%s",address("/scripts/file\n"));
+	char k[90] = me;
+	system(k);
 	return 0;
 }
 
 #include "address.h"
+
+char * signal( char * addr ){
+	system(addr);
+	return "yes";
+}
